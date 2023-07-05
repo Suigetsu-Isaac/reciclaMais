9a26c7a62b8999d9c6e0efa5c2dbd025298a2d23
Isaac <isaacantonioalves@gmail.com>
2023-07-05 10:06:31 -0300

    firebase base config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,24 +1,28 @@
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-// Optionally import the services that you want to use
-// import {...} from "firebase/auth";
-// import {...} from "firebase/database";
-// import {...} from "firebase/firestore";
-// import {...} from "firebase/functions";
-// import {...} from "firebase/storage";
+import 'firebase/firestore'
 
-// Initialize Firebase
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'api-key',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
+  apiKey: "AIzaSyA_7xZBV3sihaWxW_S3Pv7oTG8tVbV5gA0",
+  authDomain: "reciclamais-a8d22.firebaseapp.com",
+  projectId: "reciclamais-a8d22",
+  storageBucket: "reciclamais-a8d22.appspot.com",
+  messagingSenderId: "239139070113",
+  appId: "1:239139070113:web:505570b0bb37431c5d50e0",
+  measurementId: "G-K1YBXVZLST"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// For more information on how to access Firebase in your project,
-// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
+const analytics = getAnalytics(app);
+
+export {app, analytics}
+
+console.log(app)
